c76bd932be36fa188237135c08fdfc7047f26b11
Danny Hermes <dhermes@blend.com>
2020-12-15 11:52:39 -0600

    Fixing errant `clientv3` import.

--- tests/integration/proxy/grpcproxy/register_test.go
+++ tests/integration/proxy/grpcproxy/register_test.go
@@ -18,8 +18,8 @@ import (
 	"testing"
 	"time"
 
+	clientv3 "go.etcd.io/etcd/client/v3"
 	"go.etcd.io/etcd/client/v3/naming"
-	"go.etcd.io/etcd/clientv3"
 	"go.etcd.io/etcd/pkg/v3/testutil"
 	"go.etcd.io/etcd/server/v3/proxy/grpcproxy"
 	"go.etcd.io/etcd/tests/v3/integration"
